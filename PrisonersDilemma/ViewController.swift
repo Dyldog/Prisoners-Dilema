44623f61565167fd7c3e82eae03de6b4fa49acf4
Dylan Elliott <Dylan.Elliott@Gmail.com>
2017-01-08 14:42:27 +1100

    Initial commit

---
+++ PrisonersDilemma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrisonersDilemma
+//
+//  Created by Dylan Elliott on 8/1/17.
+//  Copyright © 2017 Dylan Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
